c8a99f141eec849cd555d2020bc48de5ab469627
ssms <71164978+tutorialtuts@users.noreply.github.com>
2022-02-20 13:10:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
-<?php
-// check for ip address shared internet
-if(!empty($_SERVER["HTTP_CLIENT_IP"]))
-{
-$IP = $_SERVER["HTTP_CLIENT_IP"];
-}
-else if(!empty($_SERVER["HTTP_X_FORWARDED_FOR"]))
-{
-  // check for ip address proxy servers
-   $IP = $_SERVER["HTTP_X_FORWARDED_FOR"];
-}
-else
-{
-  $IP = $_SERVER["REMOTE_ADDR"];
-}
-echo $IP;
-?>
+<html>
+  <head>
+    <title>IP FINDER</title>
+  </head>
+  <body>
+        <?php
+        // check for ip address shared internet
+        if(!empty($_SERVER["HTTP_CLIENT_IP"]))
+        {
+        $IP = $_SERVER["HTTP_CLIENT_IP"];
+        }
+        else if(!empty($_SERVER["HTTP_X_FORWARDED_FOR"]))
+        {
+          // check for ip address proxy servers
+           $IP = $_SERVER["HTTP_X_FORWARDED_FOR"];
+        }
+        else
+        {
+          $IP = $_SERVER["REMOTE_ADDR"];
+        }
+        echo $IP;
+        ?>
+  </body>
+</html>
